b9edd54ef2100a0f390dd0faa180475c237a05d3
tux1337 <33804893+tux1337@users.noreply.github.com>
2018-10-02 16:14:06 +0200

    Add files via upload

---
+++ GraphiteExportandForwarder/Metric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphiteExportandForwarder
+{
+    public class Metric
+    {
+        public int leaf;
+        public string text;
+        public int expandable;
+        public string id;
+        public int allowChildren;
+    }
+}
---
+++ GraphiteExportandForwarder/MetricData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphiteExportandForwarder
+{
+    public class MetricData
+    {
+        public string target;
+        public List<dynamic> datapoints;
+
+
+        public class Datapoint
+        {
+            public List<dynamic> data;
+        }
+    }
+}
